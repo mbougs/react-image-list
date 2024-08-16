3a7f3f07c1c6cf5562d844b8e66c99f3097c64b5
mbougs <nkanamario@yahoo.com>
2024-08-16 15:43:57 +0100

    part 2 used axios to retrieve(get) data from pixabay api

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import axios from "axios";
 import SearchInput from "./SearchInput"
 
 function App() {
-  const onSearchSubmit = (event) => {
-    console.log(event);
+  const onSearchSubmit = async (query) => {
+    const response = await axios.get(`https://pixabay.com/api/?key=45463405-0522f51c0428c9fca70bd989a&q=${query}&image_type=photo&pretty=true`)
+    console.log(response.data.hits);
+    
     
   }
   return (
